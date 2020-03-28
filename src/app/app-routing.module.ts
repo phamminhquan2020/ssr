29d86250d2dbc760c4e34fca94b05c88c8f41917
phamminhquan2020 <phamminhquan2020@gmail.com>
2020-03-28 16:29:22 +0700

    range error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { HomeModule } from './home/home.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', loadChildren: () =>
+import('./home/home.module').then(m => m.HomeModule
+  )
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, HomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
