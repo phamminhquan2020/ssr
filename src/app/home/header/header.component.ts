04fd371ba87571d5de4c88c8f1dcc2f078d3d9c2
phamminhquan2020 <phamminhquan2020@gmail.com>
2020-04-04 18:01:25 +0700

    fix fixedHeader

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -10,6 +10,25 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.fixedHeader();
+  }
+
+  fixedHeader() {
+    $(document).ready(function () {
+
+      window.onscroll = function () { myFunction() };
+
+      var header = document.getElementById("header");
+      var sticky = header.offsetTop;
+
+      function myFunction() {
+        if (window.pageYOffset >= sticky) {
+          header.classList.add("sticky")
+        } else {
+          header.classList.remove("sticky");
+        }
+      }
+    });
   }
 
 }
